39d5d223bebb16ed894d7b718c2a1d5ca52204fc
Alex286756 <ai286756@gmail.com>
2024-05-04 14:59:00 +0300

    Clear commit

--- main.py
+++
@@ -1,5 +0,0 @@
-from src.operations.simple import add
-
-if __name__ == "__main__":
-    print('Простая арифметика.')
-    print(f'2+3 = {add(2, 3)}')
--- shapes/__init__.py
+++
@@ -1,9 +0,0 @@
-from .shape import Shape
-from .circle import Circle
-from .square import Square
-
-__all__ = [
-    "Shape",
-    "Circle",
-    "Square",
-]
